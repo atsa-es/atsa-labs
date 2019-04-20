38ea25d61c35da17166f37d6a3a1b7b3514bdb43
Eli Holmes <eli.holmes@noaa.gov>
2019-04-19 17:28:34 -0700

    updating hw in marss cov chapter

--- marss-cov-lab-apr-5-update.R
+++ marss-cov-lab-apr-5-update.R
@@ -1,3 +1,8 @@
+# April 18 update
+
+[done] Need to change cov to use Temp and TP at t-1. Does not make sense
+to have temp at time t affect x(t) if we are modeling growth.
+
 # April 5th update
 
 The Temp and TP models are much worse than any seasonal model.
@@ -9,6 +14,7 @@ Note, need to switch Key to use poly() not i,i^2 etc.
 
 ord=3
 c.m.poly=t(poly(rep(1:12,15),ord))
+rownames(c.m.poly) <- paste0("Mon", 1:ord)
 C = "unconstrained"; c=c.m.poly
 model.list = c(common, list(C=C,c=c,D=D,d=d))
 test2 = MARSS(dat, model=model.list, control=ctl, silent=TRUE)
@@ -21,12 +27,14 @@ test2$AIC
 # test2 = MARSS(dat, model=model.list, control=ctl, silent=TRUE)
 # test2$AICc
 
-Instead use anomalies. Temp & TP part not explained by the polynomials. This has lower AICc.
+Instead use anomalies. Temp & TP part not explained by the polynomials. 
+This has lower AICc.
 
 fit <- lm(covars[1,]~poly(rep(1:12,15),ord))
 anoms = residuals(fit)
 fit <- lm(covars[2,]~poly(rep(1:12,15),ord))
 anoms = rbind(anoms,residuals(fit))
+rownames(anoms) <- paste0(rownames(covars)[1:2], "_anom")
 C = "unconstrained"; c=rbind(c.m.poly,anoms)
 model.list = c(common, list(C=C,c=c,D=D,d=d))
 test2 = MARSS(dat, model=model.list, control=ctl, silent=TRUE)
